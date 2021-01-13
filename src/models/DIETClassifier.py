82d18d13b9c21071a81484ceebb036bc39d87e24
WeiNyn <nguyenhuy17798@gmail.com>
2021-01-13 13:21:30 +0700

    fix num_labels not found error

--- src/models/DIETClassifier.py
+++ src/models/DIETClassifier.py
@@ -71,14 +71,14 @@ class DIETClassifier(BertPreTrainedModel):
             # Only keep active parts of the loss
             if attention_mask is not None:
                 active_loss = attention_mask.view(-1) == 1
-                active_logits = entities_logits.view(-1, self.num_labels)
+                active_logits = entities_logits.view(-1, self.num_entities)
                 active_labels = torch.where(
                     active_loss, entities_labels.view(-1),
                     torch.tensor(entities_loss_fct.ignore_index).type_as(entities_labels)
                 )
                 entities_loss = entities_loss_fct(active_logits, active_labels)
             else:
-                entities_loss = entities_loss_fct(entities_logits.view(-1, self.num_labels), entities_labels.view(-1))
+                entities_loss = entities_loss_fct(entities_logits.view(-1, self.num_entities), entities_labels.view(-1))
 
         intent_loss = None
         if intent_labels is not None:
