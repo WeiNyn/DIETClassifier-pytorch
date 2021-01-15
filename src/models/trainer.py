5365cd75ca77f4bc577c7ab252da68f7e2dbf855
WeiNyn <nguyenhuy17798@gmail.com>
2021-01-15 10:23:54 +0700

    update trainer and add EarlyStoppingCallback, TensorBoardCallback

--- src/models/trainer.py
+++ src/models/trainer.py
@@ -1,4 +1,7 @@
-from transformers import Trainer, TrainingArguments
+from typing import Optional
+
+from transformers import Trainer, TrainingArguments, EarlyStoppingCallback
+from trainsformers.integrations import TensorBoardCallback
 from torch.utils.data import random_split
 import torch
 
@@ -6,7 +9,7 @@ import torch
 class DIETTrainer:
     def __init__(self, model, dataset, train_range: 0.95, output_dir: str = "results", num_train_epochs: int = 100, per_device_train_batch_size: int = 4,
                  per_device_eval_batch_size: int = 4, warmup_steps: int = 500, weight_decay: float = 0.01,
-                 logging_dir: str = "logs"):
+                 logging_dir: str = "logs", early_stopping_patience: int = 20, early_stopping_threshold: float = 1e-5):
         """
         Create DIETTrainer class
 
@@ -21,10 +24,19 @@ class DIETTrainer:
         :param weight_decay: weight decay
         :param logging_dir: logging directory
         """
-        self.training_args = TrainingArguments(output_dir=output_dir, num_train_epochs=num_train_epochs,
-                                          per_device_train_batch_size=per_device_train_batch_size,
-                                          per_device_eval_batch_size=per_device_eval_batch_size,
-                                          warmup_steps=warmup_steps, weight_decay=weight_decay, logging_dir=logging_dir)
+        self.training_args = TrainingArguments(output_dir=output_dir,
+                                               num_train_epochs=num_train_epochs,
+                                               per_device_train_batch_size=per_device_train_batch_size,
+                                               per_device_eval_batch_size=per_device_eval_batch_size,
+                                               warmup_steps=warmup_steps,
+                                               weight_decay=weight_decay,
+                                               logging_dir=logging_dir,
+                                               load_best_model_at_end=True,
+                                               metric_for_best_model="loss",
+                                               greater_is_better=False,
+                                               evaluation_strategy="epoch",
+                                               label_names=["entities_labels", "intent_labels"],
+                                               save_total_limit=1)
 
         train_dataset, eval_dataset = random_split(dataset, [int(len(dataset)*train_range), len(dataset) - int(len(dataset)*train_range)], generator=torch.Generator().manual_seed(42))
 
@@ -33,6 +45,7 @@ class DIETTrainer:
             args=self.training_args,
             train_dataset=train_dataset,
             eval_dataset=eval_dataset,
+            callbacks=[EarlyStoppingCallback(early_stopping_patience=early_stopping_patience, early_stopping_threshold=early_stopping_threshold), TensorBoardCallback()]
         )
 
     def train(self):
@@ -51,6 +64,8 @@ if __name__ == '__main__':
     from transformers import AutoTokenizer
     from transformers.configuration_utils import PretrainedConfig
 
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
     files = ["Dataset/nlu_QnA_converted.yml", "Dataset/nlu_QnA_converted.yml"]
     tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
 
@@ -63,6 +78,7 @@ if __name__ == '__main__':
     sentences = ["What if I'm late"]
 
     inputs = tokenizer(sentences, return_tensors="pt", padding="max_length", max_length=512)
+
     outputs = model(**{k: v for k, v in inputs.items()})
 
     trainer = DIETTrainer(model=model, dataset=dataset, train_range=0.95)
